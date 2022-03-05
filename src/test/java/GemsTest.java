1ad9e5591d6f74db62e537494ebd9f08fe9ac77c
AndreiYablkovskiy <t3n7ai@gmail.com>
2022-03-05 22:54:26 +0300

    Fixed code repetition in tests

--- src/test/java/GemsTest.java
+++ src/test/java/GemsTest.java
@@ -6,6 +6,10 @@ public class GemsTest {
     @Test
     public void getGemPriceTest (){
         Gem brilliant = new Brilliant(3, 30.5, 150.0);
-        Assert.assertEquals(675, brilliant.getGemPrice(), 0);
+        Gem ruby = new Ruby(5, 15, 10);
+        Gem topaz = new Topaz(4, 24, 15);
+        Assert.assertEquals(675,brilliant.getGemPrice(),0);
+        Assert.assertEquals(75,ruby.getGemPrice(),0);
+        Assert.assertEquals(90,topaz.getGemPrice(),0);
     }
 }
--- src/test/java/NecklaceTest.java
+++ src/test/java/NecklaceTest.java
@@ -1,100 +1,67 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class NecklaceTest {
+    private Gem brilliant;
+    private Gem ruby;
+    private Gem topaz;
+    private Necklace necklace;
 
-    @Test
-    public void getJewelPriceTest () {
-        Gem brilliant = new Brilliant(3, 30.5, 150.0);
-        Gem ruby = new Ruby(5, 15, 10);
-        Gem topaz = new Topaz(4, 24, 15);
-
+    @Before
+    public void init() {
+        brilliant = new Brilliant(3, 30.5, 150.0);
+        ruby = new Ruby(5, 15, 10);
+        topaz = new Topaz(4, 24, 15);
         List<Gem> list = new ArrayList<>();
         list.add(brilliant);
         list.add(ruby);
         list.add(topaz);
-        Necklace necklace = new Necklace(list);
+        necklace = new Necklace(list);
+    }
 
+    @Test
+    public void necklaceGemsListTest(){
+        Assert.assertEquals(3,necklace.getGemsList().size());
+        Assert.assertEquals(brilliant,necklace.getGemsList().get(0));
+        Assert.assertEquals(ruby,necklace.getGemsList().get(1));
+        Assert.assertEquals(topaz,necklace.getGemsList().get(2));
+    }
+
+    @Test
+    public void getJewelPriceTest() {
         Assert.assertEquals(840, necklace.getJewelPrice(), 0);
     }
 
     @Test
-    public void getJewelCaratValueTest (){
-        Gem brilliant = new Brilliant(3, 30.5, 150.0);
-        Gem ruby = new Ruby(5, 15, 10);
-        Gem topaz = new Topaz(4, 24, 15);
-
-        List<Gem> list = new ArrayList<>();
-        list.add(brilliant);
-        list.add(ruby);
-        list.add(topaz);
-        Necklace necklace = new Necklace(list);
-
+    public void getJewelCaratValueTest() {
         Assert.assertEquals(12, necklace.getJewelCaratValue(), 0);
     }
 
     @Test
-    public void sortLowToHighPriceTest (){
-        Gem brilliant = new Brilliant(3, 30.5, 150.0);
-        Gem ruby = new Ruby(5, 15, 10);
-        Gem topaz = new Topaz(4, 24, 15);
-
-        List<Gem> expectedList = new ArrayList<>();
-        expectedList.add(ruby);
-        expectedList.add(topaz);
-        expectedList.add(brilliant);
-
-        List<Gem> list = new ArrayList<>();
-        list.add(brilliant);
-        list.add(ruby);
-        list.add(topaz);
-        Necklace necklace = new Necklace(list);
+    public void sortLowToHighPriceTest() {
         necklace.sortLowToHighPrice();
-
-        Assert.assertEquals(expectedList,necklace.getGemsList());
+        Assert.assertEquals(ruby, necklace.getGemsList().get(0));
+        Assert.assertEquals(topaz, necklace.getGemsList().get(1));
+        Assert.assertEquals(brilliant, necklace.getGemsList().get(2));
     }
 
     @Test
-    public void sortHighToLowPriceTest (){
-        Gem brilliant = new Brilliant(3, 30.5, 150.0);
-        Gem ruby = new Ruby(5, 15, 10);
-        Gem topaz = new Topaz(4, 24, 15);
-
-        List<Gem> expectedList = new ArrayList<>();
-        expectedList.add(brilliant);
-        expectedList.add(topaz);
-        expectedList.add(ruby);
-
-        List<Gem> list = new ArrayList<>();
-        list.add(brilliant);
-        list.add(ruby);
-        list.add(topaz);
-        Necklace necklace = new Necklace(list);
+    public void sortHighToLowPriceTest() {
         necklace.sortHighToLowPrice();
-
-        Assert.assertEquals(expectedList,necklace.getGemsList());
+        Assert.assertEquals(brilliant, necklace.getGemsList().get(0));
+        Assert.assertEquals(topaz, necklace.getGemsList().get(1));
+        Assert.assertEquals(ruby, necklace.getGemsList().get(2));
     }
 
     @Test
-    public void searchByClarityTest (){
-        Gem brilliant = new Brilliant(3, 30.5, 150.0);
-        Gem ruby = new Ruby(5, 15, 10);
-        Gem topaz = new Topaz(4, 24, 15);
-
-        List<Gem> expectedList = new ArrayList<>();
-        expectedList.add(ruby);
-        expectedList.add(topaz);
-
-        List<Gem> list = new ArrayList<>();
-        list.add(brilliant);
-        list.add(ruby);
-        list.add(topaz);
-        Necklace necklace = new Necklace(list);
-
-        Assert.assertEquals(expectedList,necklace.searchByClarity(12,25));
+    public void searchByClarityTest() {
+        List<Gem> list = necklace.searchByClarity(12, 25);
+        Assert.assertEquals(2, list.size());
+        Assert.assertEquals(ruby, list.get(0));
+        Assert.assertEquals(topaz, list.get(1));
     }
-
 }
